98956111d7c78747adeea3ff710e444894a14a6b
Michael Vargas <Michael.Vargas0290@gmail.com>
2022-09-15 19:49:30 -0500

    Create Footer Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header.js';
+import Footer from './components/Footer/Footer.js';
 import './App.css';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
   return (
     <div className='App'>
       <Header />
-
+      <Footer />
     </div>
   )
 }
