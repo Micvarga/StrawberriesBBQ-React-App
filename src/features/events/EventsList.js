924fda53f56a0bb25a358f7ed6308a2788df7c81
Michael Vargas <Michael.Vargas0290@gmail.com>
2022-10-26 18:51:26 -0500

    Update EventCard component.

---
+++ src/features/events/EventsList.js
@@ -0,0 +1,17 @@
+import { MONTHLYEVENTS } from '../../app/assets/App Data/MONTHLYEVENTS';
+import { Col, Row } from 'reactstrap';
+import EventCard from '../events/EventCard';
+
+const EventsList = () => {
+    <Row className='ms-auto'>
+        {MONTHLYEVENTS.map((event) => {
+            return (
+                <Col md='5' className='m-4' key={event.id}>
+                    <EventCard event={event} />
+                </Col>
+            );
+        })}
+    </Row>
+};
+
+export default EventsList;
