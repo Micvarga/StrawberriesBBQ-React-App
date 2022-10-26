c214810dd713fc2c79d6290d159f14022277d5e9
Michael Vargas <Michael.Vargas0290@gmail.com>
2022-10-26 18:50:59 -0500

    Add EventsList to Homepage component.

--- src/pages/Home Page/HomePage.js
+++ src/pages/Home Page/HomePage.js
@@ -1,11 +1,18 @@
 import CarouselComponent from "../../components/Carousel/Carousel";
 import { Container } from "reactstrap";
+import EventsList from "../../features/events/EventsList";
 
 const HomePage = () => {
     return (
-        <Container>
-            <CarouselComponent />
-        </Container>
+        <>
+            <Container>
+                <CarouselComponent />
+            </Container>
+            <Container>
+                <EventsList />
+            </Container>
+        </>
+
 
     )
 };
