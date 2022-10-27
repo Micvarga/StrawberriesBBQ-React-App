4294687398c3f1790d48b94ea6e6f560b2f4b0f4
Michael Vargas <Michael.Vargas0290@gmail.com>
2022-10-27 15:27:39 -0500

    Update MenuPage with MenuItemsList component.

---
+++ src/pages/Menu Page/MenuPage.js
@@ -0,0 +1,12 @@
+import { Container } from "reactstrap";
+import MenuItemsList from "../../features/menu/MenuItemsList";
+
+const MenuPage = () => {
+    return (
+        <Container>
+            <MenuItemsList />
+        </Container>
+    )
+};
+
+export default MenuPage;
