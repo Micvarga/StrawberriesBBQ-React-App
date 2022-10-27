96bb1ee604627dbea9b804c9e46aeeb0145d8d71
Michael Vargas <Michael.Vargas0290@gmail.com>
2022-10-27 14:16:36 -0500

    Create MENUITEMS arrays.

---
+++ src/app/assets/App Data/MENUITEMS.js
@@ -0,0 +1,77 @@
+export const MEATITEMS = () => [
+    {
+        id: 0,
+        name: 'Pork Spare Ribs',
+        description: 'Slow smoked and slathered in our house sauce, "Lost in the Sauce".',
+        price: '1/4 , 1/2, Full Rack...............$10/16/28',
+    },
+    {
+        id: 1,
+        name: 'Chopped Brisket Sandwich',
+        descrption: 'Covered in our Coffee Rub, smoked 18 hours.',
+        price: 'Sandwich...............$14',
+    },
+    {
+        id: 2,
+        name: 'Pulled Pork Sandwhich',
+        description: 'Chopped pork shoulder, smoked slow and low.',
+        price: 'Sandwich...............$12',
+    },
+    {
+        id: 3,
+        name: 'Smoked Bologna Sandwhich',
+        description: 'Sliced bologna, smoked slow and low.',
+        price: 'Sandwich...............$8',
+    }
+];
+
+export const SHAREABLEITEMS = () => [
+    {
+        id: 0,
+        name: 'Pork Belly Burn Ends',
+        description: 'Crisp pork belly burn ends covered in house',
+        price: '1/2 lb...............$10',
+    },
+    {
+        id: 1,
+        name: 'Smoked Texas Hot Links',
+        description: 'Hot and Spicey like the Texas sun',
+        price: 'per link...............$7',
+    },
+];
+
+export const SIDEITEMS = () => [
+    {
+        id: 0,
+        name: 'Mac N Cheese',
+        description: 'Thich and cheese. Better than the blue box stuff.',
+        price: 'per cup...............$4',
+    },
+    {
+        id: 1,
+        name: 'Corn Bake',
+        description: 'Homemade delicious corn bake.',
+        price: ' per slice...............$4',
+    },
+    {
+        id: 2,
+        name: 'Spicey Cole Slaw',
+        description: 'Cole Slaw with a Strawberries BBQ kick!',
+        price: 'per slice...............$4',
+    },
+];
+
+export const DESSERTITEMS = () => [
+    {
+        id: 0,
+        name: 'Smoked Apple Pie',
+        description: 'Delicious apple pie slow smoked in our smoker.',
+        price: 'per slice...............$4',
+    },
+    {
+        id: 1,
+        name: `Snickerdoodle S'More.`,
+        description: 'Our in house creation. Thick gooey and delicious!',
+        price: `per S'More ...............$7`,
+    },
+];
