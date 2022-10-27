6cd61737e588a093864bc702e6f8f021a8a5b266
Michael Vargas <Michael.Vargas0290@gmail.com>
2022-10-27 15:27:11 -0500

    Update routes.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header/Header.js';
 import Footer from './components/Footer/Footer.js';
 import ContactUsPage from './pages/Contact Us/ContactUs';
 import HomePage from './pages/Home Page/HomePage.js';
+import MenuPage from './pages/Menu Page/MenuPage.js';
 import './App.css';
 
 
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='contact' element={<ContactUsPage />} />
+        <Route path='menu' element={<MenuPage />} />
       </Routes>
       <Footer />
     </div>
