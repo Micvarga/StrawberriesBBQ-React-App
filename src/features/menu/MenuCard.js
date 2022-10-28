7104962591868a043ee98e45ffb016a50d3a5e07
Michael Vargas <Michael.Vargas0290@gmail.com>
2022-10-28 09:54:11 -0500

    Update MenuCard component.

--- src/features/menu/MenuCard.js
+++ src/features/menu/MenuCard.js
@@ -1,16 +1,19 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCamera } from '@fortawesome/free-solid-svg-icons';
+import { Col, Row } from 'reactstrap';
+import './MenuStyles.css';
 
 const MenuCard = ({ menuItem }) => {
     const { name, description, price } = menuItem;
     return (
-        <div>
-            <h5 className='cardTitle'>{name}</h5>
-            <p className='cardSubtitle'>{description}</p>
-            <p className='cardSubtitle'>{price}<a className='btn'>
-                <FontAwesomeIcon icon={faCamera} size='lg' />
-            </a></p>
-        </div>
+        <Row>
+            <Col sm='12' className='menuContent'>
+                <h3 className='menuCardTitle'>{name}</h3>
+                <h4 className='menuCardSubtitle'>{description}  {price}<a className='btn'>
+                    <FontAwesomeIcon icon={faCamera} size='lg' />
+                </a></h4>
+            </Col>
+        </Row>
     );
 }
 
