6149fd9873a65feaba93e6ee5c56cc7f41bfb3f9
Michael Vargas <Michael.Vargas0290@gmail.com>
2022-10-31 18:13:43 -0500

    Add className to elements to help resolve styling bug.

--- src/features/team info/TeamInfo.js
+++ src/features/team info/TeamInfo.js
@@ -7,14 +7,14 @@ const TeamInfo = () => {
         <Container className="teamInfoContent">
             <Row>
                 <Col sm='12'>
-                    <h1>About Strawberries BBQ</h1>
+                    <h1 className="teamInfoSubTitle">About Strawberries BBQ</h1>
                     <p className="companyStoryText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta adipisci et vitae ratione
                         perspiciatis laudantium assumenda alias, corrupti totam illo vel quia ad rerum ipsa quasi, ipsum
                         neque repellendus sequi.</p>
                 </Col>
             </Row>
             <div className="mb-5 mt-5">
-                <h2>The Team</h2>
+                <h2 className="teamInfoSubTitle">The Team</h2>
             </div>
             <Row>
                 <Col sm='12' md='6' className="order-1 mb-5">
