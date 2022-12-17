8dc86575b28a3e993753b17fe2eb04fbd26e9ae6
Michael Vargas <Michael.Vargas0290@gmail.com>
2022-12-16 19:15:53 -0600

    Add image property and links to MENUITEMS array

--- src/app/assets/App Data/MENUITEMS.js
+++ src/app/assets/App Data/MENUITEMS.js
@@ -1,4 +1,14 @@
 import porkSpareRibsImg from '../img/porkSpareRibs.jpg';
+import brisketSandwich from '../img/brisketSandwich.jpg';
+import pulledPorkSandwich from '../../assets/img/pulledPorkSandwich.jpg';
+import smokedBolognaSandwich from '../../assets/img/smokedBolognaSandwich.jpg';
+import porkBellyBurntEnds from '../../assets/img/porkBellyBurntEnds.jpg';
+import hotLinks from '../../assets/img/hotLinks.jpg';
+import macAndCheese from '../../assets/img/macAndCheese.jpg';
+import cornBake from '../../assets/img/cornBake.jpg';
+import coleSlaw from '../img/coleSlaw.jpg';
+import applePie from '../img/applePie.jpg';
+import snickerdoodleSmore from '../img/snickerdoodleSmore.png';
 
 
 export const MENUITEMS = [
@@ -6,7 +16,7 @@ export const MENUITEMS = [
         id: 0,
         type: 'meats',
         name: 'Pork Spare Ribs',
-        image: 'porkSpareRibsImg',
+        image: porkSpareRibsImg,
         description: 'Slow smoked and slathered in our house sauce, "Lost in the Sauce".',
         price: '1/4 , 1/2, Full Rack...............$10/16/28',
     },
@@ -14,7 +24,7 @@ export const MENUITEMS = [
         id: 1,
         type: 'meats',
         name: 'Chopped Brisket Sandwich',
-        image: 'porkSpareRibsImg',
+        image: brisketSandwich,
         description: 'Covered in our Coffee Rub, smoked 18 hours.',
         price: 'per sandwich...............$14',
     },
@@ -22,7 +32,7 @@ export const MENUITEMS = [
         id: 2,
         type: 'meats',
         name: 'Pulled Pork Sandwhich',
-        image: 'porkSpareRibsImg',
+        image: pulledPorkSandwich,
         description: 'Chopped pork shoulder, smoked slow and low.',
         price: 'per sandwich...............$12',
     },
@@ -30,7 +40,7 @@ export const MENUITEMS = [
         id: 3,
         type: 'meats',
         name: 'Smoked Bologna Sandwhich',
-        image: 'porkSpareRibsImg',
+        image: smokedBolognaSandwich,
         description: 'Sliced bologna, smoked slow and low.',
         price: 'per sandwich...............$8',
     },
@@ -38,7 +48,7 @@ export const MENUITEMS = [
         id: 0,
         type: 'shareable',
         name: 'Pork Belly Burn Ends',
-        image: 'porkSpareRibsImg',
+        image: porkBellyBurntEnds,
         description: 'Crisp pork belly burn ends covered in house sauce.',
         price: '1/2 lb...............$10',
     },
@@ -46,7 +56,7 @@ export const MENUITEMS = [
         id: 1,
         type: 'shareable',
         name: 'Smoked Texas Hot Links',
-        image: 'porkSpareRibsImg',
+        image: hotLinks,
         description: 'Hot and Spicey like the Texas sun!',
         price: 'per link...............$7',
     },
@@ -54,7 +64,7 @@ export const MENUITEMS = [
         id: 0,
         type: 'sides',
         name: 'Mac N Cheese',
-        image: 'porkSpareRibsImg',
+        image: macAndCheese,
         description: 'Thich and cheese. Better than the blue box stuff.',
         price: 'per cup...............$4',
     },
@@ -62,7 +72,7 @@ export const MENUITEMS = [
         id: 1,
         type: 'sides',
         name: 'Corn Bake',
-        image: 'porkSpareRibsImg',
+        image: cornBake,
         description: 'Homemade delicious corn bake.',
         price: ' per slice...............$4',
     },
@@ -70,7 +80,7 @@ export const MENUITEMS = [
         id: 2,
         type: 'sides',
         name: 'Spicey Cole Slaw',
-        image: 'porkSpareRibsImg',
+        image: coleSlaw,
         description: 'Cole Slaw with a Strawberries BBQ kick!',
         price: 'per slice...............$4',
     },
@@ -79,7 +89,7 @@ export const MENUITEMS = [
         id: 0,
         type: 'desserts',
         name: 'Smoked Apple Pie',
-        image: 'porkSpareRibsImg',
+        image: applePie,
         description: 'Delicious apple pie slow smoked in our smoker.',
         price: 'per slice...............$4',
     },
@@ -87,7 +97,7 @@ export const MENUITEMS = [
         id: 1,
         type: 'desserts',
         name: `Snickerdoodle S'More.`,
-        image: 'porkSpareRibsImg',
+        image: snickerdoodleSmore,
         description: 'Our in house creation. Thick gooey and delicious!',
         price: `per S'More ...............$7`,
     },
