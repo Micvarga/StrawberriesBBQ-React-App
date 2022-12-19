806c591907f01f1d4ed32bc3f2738b3fe2894a00
Michael Vargas <Michael.Vargas0290@gmail.com>
2022-12-19 14:36:14 -0600

    Add Menu Page link to Hero section

--- src/components/Hero/MenuHero/MenuHero.js
+++ src/components/Hero/MenuHero/MenuHero.js
@@ -1,13 +1,16 @@
+import { Link } from 'react-router-dom';
 import './MenuHero.css';
 
 const MenuHero = () => {
     return (
         <section className="menuHeroSection">
             <div className="menuHeroText">
-                <h1>Menu</h1>
-                <p>
-                    Check out our awesome menu of delicious BBQ!
-                </p>
+                <Link to='/menu' className='menuHeroLink'>
+                    <h1>Menu</h1>
+                    <p>
+                        Check out our awesome menu of delicious BBQ!
+                    </p>
+                </Link>
             </div>
         </section>
     )
