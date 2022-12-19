514e2c37cf61086213fd1529f3d0b93e4548a831
Michael Vargas <Michael.Vargas0290@gmail.com>
2022-12-19 13:06:02 -0600

    Create MenuHero Component

---
+++ src/components/Hero/MenuHero/MenuHero.js
@@ -0,0 +1,17 @@
+import './MenuHero.css';
+
+const MenuHero = () => {
+    return (
+        <section className="menuHeroSection">
+            <div className="menuHeroText">
+                <h1>Menu</h1>
+                <p>
+                    Check out our awesome menu of delicious BBQ!
+                </p>
+            </div>
+        </section>
+    )
+};
+
+export default MenuHero;
+
--- src/pages/Home Page/HomePage.js
+++ src/pages/Home Page/HomePage.js
@@ -1,11 +1,14 @@
 import CarouselComponent from "../../components/Carousel/Carousel";
 import { Container } from "reactstrap";
 import EventsList from "../../features/events/EventsList";
+import MenuHero from "../../components/Hero/MenuHero/MenuHero";
+
 
 const HomePage = () => {
     return (
         <>
             <CarouselComponent />
+            <MenuHero />
             <Container>
                 <EventsList />
             </Container>
