0fdc45477db62b83768b4f3e1bd026083f72c1b8
Michael Vargas <Michael.Vargas0290@gmail.com>
2022-12-19 14:35:58 -0600

    Add Contact Us page link to Hero section

--- src/components/Hero/ContactUsHero/ContactUsHero.js
+++ src/components/Hero/ContactUsHero/ContactUsHero.js
@@ -1,13 +1,16 @@
+import { Link } from 'react-router-dom';
 import './ContactUsHero.css';
 
 const ContactUsHero = () => {
     return (
         <section className="contactUsHeroSection">
             <div className="contactUsHeroText">
-                <h1>Contact Us</h1>
-                <p>
-                    Request us for your next event or sign up for our newsletter!
-                </p>
+                <Link to='/contact' className='contactUsHeroLink'>
+                    <h1>Contact Us</h1>
+                    <p>
+                        Request us for your next event or sign up for our newsletter!
+                    </p>
+                </Link>
             </div>
         </section>
     )
