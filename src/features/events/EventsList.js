eddf1b4e820695b4134e29ac57eeb2c6cead36cd
Michael Vargas <Michael.Vargas0290@gmail.com>
2022-12-21 13:35:50 -0600

    Create eventsSlice

--- src/features/events/EventsList.js
+++ src/features/events/EventsList.js
@@ -1,12 +1,14 @@
-import { MONTHLYEVENTS } from '../../app/assets/App Data/MONTHLYEVENTS';
+import { selectAllMonthlyEvents } from './eventsSlice';
 import { Col, Row } from 'reactstrap';
 import EventCard from '../events/EventCard';
 import './EventStyles.css';
 
 const EventsList = () => {
+    const monthlyEvents = selectAllMonthlyEvents();
+
     return (
         <Row className='eventsList'>
-            {MONTHLYEVENTS.map((event) => {
+            {monthlyEvents.map((event) => {
                 return (
                     <Col xs='12' lg='6' key={event.id}>
                         <EventCard event={event} />
---
+++ src/features/events/eventsSlice.js
@@ -0,0 +1,5 @@
+import { MONTHLYEVENTS } from "../../app/assets/App Data/MONTHLYEVENTS";
+
+export const selectAllMonthlyEvents = () => {
+    return MONTHLYEVENTS;
+};
