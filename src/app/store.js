2018db3d0fdc867f17e766659b0c0eee15c54245
Michael Vargas <Michael.Vargas0290@gmail.com>
2022-12-23 15:54:06 -0600

    Add menuItemsReduce to store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import { menuItemsReducer } from '../features/menu/menuSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    menuItems: menuItemsReducer
   },
 });
