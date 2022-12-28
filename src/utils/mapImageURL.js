3882a44f0e7a6155e6e1534b7250e43f95740697
Michael Vargas <Michael.Vargas0290@gmail.com>
2022-12-27 21:28:56 -0600

    Update mapImageUrl image location.

--- src/utils/mapImageURL.js
+++ src/utils/mapImageURL.js
@@ -1,21 +1,21 @@
-import { baseUrl } from "../app/assets/App Data/baseUrl";
+
 
 
 export const mapImageURL = (arr) => {
     return arr.map((item) => {
         return {
             ...item,
-            image: baseUrl + item.image
+            image: require("../app/assets/img/" + item.image)
         };
     });
 };
 
-export const mapCarouselImageURL = (arr) => {
-    return arr.map((item) => {
-        return {
-            ...item,
-            src: baseUrl + item.src
-        };
-    });
-};
+// export const mapCarouselImageURL = (arr) => {
+//     return arr.map((item) => {
+//         return {
+//             ...item,
+//             src: baseUrl + item.src
+//         };
+//     });
+// };
 
