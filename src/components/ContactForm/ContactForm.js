46ca2a7c53d6191a52320c4d475687a116c0d527
Michael Vargas <Michael.Vargas0290@gmail.com>
2022-12-29 14:26:10 -0600

    Update contact form field names and add emailjs.send function

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,9 +1,23 @@
 import { Button, Label, Col, FormGroup } from 'reactstrap';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { useRef } from 'react';
 import { validateContactForm } from '../../utils/validateContactForm';
+import emailjs from '@emailjs/browser';
+
 
 const ContactForm = () => {
+
+    const sendEmail = (object) => {
+        emailjs.send(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, object, process.env.REACT_APP_PUBLIC_KEY)
+            .then((result) => {
+                console.log(result.text)
+            }, (error) => {
+                console.log(error.text)
+            })
+    };
+
     const handleSubmit = (values, { resetForm }) => {
+        sendEmail(values);
         console.log('form values:', values);
         console.log('in JSON format:', JSON.stringify(values));
         resetForm();
@@ -11,59 +25,41 @@ const ContactForm = () => {
     return (
         <Formik
             initialValues={{
-                firstName: '',
-                lastName: '',
-                phoneNum: '',
-                email: '',
-                agree: false,
-                contactType: 'By Phone',
-                feedback: ''
+                client_name: '',
+                client_phone: '',
+                client_email: '',
+                message: ''
             }}
             onSubmit={handleSubmit}
             validate={validateContactForm}
         >
             <Form>
                 <FormGroup row>
-                    <Label htmlFor='firstName' md='2'>
-                        First Name
+                    <Label htmlFor='client_name' md='2'>
+                        Name
                     </Label>
                     <Col md='10'>
                         <Field
-                            name='firstName'
-                            placeholder='First Name'
+                            name='client_name'
+                            placeholder='Your Name'
                             className='form-control'
                         />
-                        <ErrorMessage name='firstName'>
+                        <ErrorMessage name='client_name'>
                             {(msg) => <p className='text-danger'>{msg}</p>}
                         </ErrorMessage>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
-                    <Label htmlFor='lastName' md='2'>
-                        Last Name
-                    </Label>
-                    <Col md='10'>
-                        <Field
-                            name='lastName'
-                            placeholder='Last Name'
-                            className='form-control'
-                        />
-                        <ErrorMessage name='lastName'>
-                            {(msg) => <p className='text-danger'>{msg}</p>}
-                        </ErrorMessage>
-                    </Col>
-                </FormGroup>
-                <FormGroup row>
-                    <Label htmlFor='phoneNum' md='2'>
+                    <Label htmlFor='client_phone' md='2'>
                         Phone
                     </Label>
                     <Col md='10'>
                         <Field
-                            name='phoneNum'
-                            placeholder='Phone'
+                            name='client_phone'
+                            placeholder='Your phone number'
                             className='form-control'
                         />
-                        <ErrorMessage name='phoneNum'>
+                        <ErrorMessage name='client_phone'>
                             {(msg) => <p className='text-danger'>{msg}</p>}
                         </ErrorMessage>
                     </Col>
@@ -74,43 +70,24 @@ const ContactForm = () => {
                     </Label>
                     <Col md='10'>
                         <Field
-                            name='email'
-                            placeholder='Email'
+                            name='client_email'
+                            placeholder='Your email'
                             type='email'
                             className='form-control'
                         />
-                        <ErrorMessage name='email'>
+                        <ErrorMessage name='client_email'>
                             {(msg) => <p className='text-danger'>{msg}</p>}
                         </ErrorMessage>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
-                    <Label check md={{ size: 4, offset: 2 }}>
-                        <Field
-                            name='agree'
-                            type='checkbox'
-                            className='form-check-input'
-                        />{' '}
-                        May we contact you?
-                    </Label>
-                    <Col md='4'>
-                        <Field
-                            name='contactType'
-                            as='select'
-                            className='form-control'
-                        >
-                            <option>By Phone</option>
-                            <option>By Email</option>
-                        </Field>
-                    </Col>
-                </FormGroup>
-                <FormGroup row>
-                    <Label htmlFor='feedback' md='2'>
-                        Your Feedback
+                    <Label htmlFor='message' md='2'>
+                        Event details
                     </Label>
                     <Col md='10'>
                         <Field
-                            name='feedback'
+                            name='message'
+                            placeholder='Enter all event details here.'
                             as='textarea'
                             rows='12'
                             className='form-control'
@@ -120,7 +97,7 @@ const ContactForm = () => {
                 <FormGroup row>
                     <Col md={{ size: '10', offset: '2' }}>
                         <Button type='submit' color='primary' >
-                            Send Feedback
+                            Send Request
                         </Button>
                     </Col>
                 </FormGroup>
