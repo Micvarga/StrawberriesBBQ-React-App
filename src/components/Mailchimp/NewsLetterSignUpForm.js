1cf0033da9786a79909fb041fa241c4a9cafaa30
Michael Vargas <Michael.Vargas0290@gmail.com>
2022-12-31 16:21:50 -0600

    Remove Formik and add custom controlled form with link to MailChimp

--- src/components/Mailchimp/NewsLetterSignUpForm.js
+++ src/components/Mailchimp/NewsLetterSignUpForm.js
@@ -1,79 +1,93 @@
-import { Button, Label, Col, FormGroup } from 'reactstrap';
-import { Formik, Field, Form, ErrorMessage } from 'formik';
-import { validateNewsLetterForm } from '../../utils/validateNewsLetterForm';
+import { Button, Label, Col, FormGroup, Input, Form } from 'reactstrap';
+import { useState } from 'react';
 
-const NewsLetterSignUpForm = () => {
-    const handleSubmit = (values, { resetForm }) => {
-        console.log('form values:', values);
-        console.log('in JSON format:', JSON.stringify(values));
-        resetForm();
+const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: ""
+}
+
+const NewsLetterSignUpForm = ({ status, message, onValidated }) => {
+
+    const [formValues, updateFormValues] = useState(initialValues);
+    const { firstName, lastName, email } = formValues;
+
+    const handleChange = (e) => {
+        updateFormValues({
+            ...formValues,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formValues);
+        email &&
+            firstName &&
+            lastName &&
+            email.indexOf("@") > -1 &&
+            onValidated({
+                EMAIL: email,
+                MERGE1: firstName,
+                MERGE2: lastName,
+            });
     };
     return (
-        <Formik
-            initialValues={{
-                email: '',
-                firstName: '',
-                lastName: ''
-            }}
-            onSubmit={handleSubmit}
-            validate={validateNewsLetterForm}
-        >
-            <Form>
-                <FormGroup row>
-                    <Label htmlFor='email' md='2'>
-                        Email
-                    </Label>
-                    <Col md='10'>
-                        <Field
-                            name='email'
-                            placeholder='Email'
-                            type='email'
-                            className='form-control'
-                        />
-                        <ErrorMessage name='email'>
-                            {(msg) => <p className='text-danger'>{msg}</p>}
-                        </ErrorMessage>
-                    </Col>
-                </FormGroup>
-                <FormGroup row>
-                    <Label htmlFor='firstName' md='2'>
-                        First Name
-                    </Label>
-                    <Col md='10'>
-                        <Field
-                            name='firstName'
-                            placeholder='First Name'
-                            className='form-control'
-                        />
-                        <ErrorMessage name='firstName'>
-                            {(msg) => <p className='text-danger'>{msg}</p>}
-                        </ErrorMessage>
-                    </Col>
-                </FormGroup>
-                <FormGroup row>
-                    <Label htmlFor='lastName' md='2'>
-                        Last Name
-                    </Label>
-                    <Col md='10'>
-                        <Field
-                            name='lastName'
-                            placeholder='Last Name'
-                            className='form-control'
-                        />
-                        <ErrorMessage name='lastName'>
-                            {(msg) => <p className='text-danger'>{msg}</p>}
-                        </ErrorMessage>
-                    </Col>
-                </FormGroup>
-                <FormGroup row>
-                    <Col md={{ size: '10', offset: '2' }}>
-                        <Button type='submit' color='primary' >
-                            Subscribe
-                        </Button>
-                    </Col>
-                </FormGroup>
-            </Form>
-        </Formik>
+
+        <Form onSubmit={(e) => handleSubmit(e)}>
+            <FormGroup row>
+                <Label for='email' md='2'>
+                    Email
+                </Label>
+                <Col md='10'>
+                    <Input
+                        id='email'
+                        name='email'
+                        placeholder='Your Email Address'
+                        type='email'
+                        value={email}
+                        onChange={handleChange}
+                    />
+                </Col>
+            </FormGroup>
+            <FormGroup row>
+                <Label for='firstName' md='2'>
+                    First Name
+                </Label>
+                <Col md='10'>
+                    <Input
+                        id='firstName'
+                        name='firstName'
+                        placeholder='John'
+                        type='text'
+                        value={firstName}
+                        onChange={handleChange}
+                    />
+                </Col>
+            </FormGroup>
+            <FormGroup row>
+                <Label for='lastName' md='2'>
+                    Last Name
+                </Label>
+                <Col md='10'>
+                    <Input
+                        id='lastName'
+                        name='lastName'
+                        placeholder='Doe'
+                        type='text'
+                        value={lastName}
+                        onChange={handleChange}
+                    />
+                </Col>
+            </FormGroup>
+            <FormGroup row>
+                <Col md={{ size: '10', offset: '2' }}>
+                    <Button type='submit' color='primary' >
+                        Subscribe
+                    </Button>
+                </Col>
+            </FormGroup>
+        </Form>
     )
 };
 
