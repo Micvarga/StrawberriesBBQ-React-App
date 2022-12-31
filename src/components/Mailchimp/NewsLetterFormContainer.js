15e38f6ed6ec4cbc286f90d1462c82cd69f7e817
Michael Vargas <Michael.Vargas0290@gmail.com>
2022-12-31 16:20:32 -0600

    Create NewsLetterFromContainer

---
+++ src/components/Mailchimp/NewsLetterFormContainer.js
@@ -0,0 +1,24 @@
+import MailchimpSubscribe from "react-mailchimp-subscribe";
+import NewsLetterSignUpForm from "./NewsLetterSignUpForm";
+
+const NewsLetterFormContainer = (props) => {
+
+    const postUrl = `https://COM.us14.list-manage.com/subscribe/post?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
+
+    return (
+        <div>
+            <MailchimpSubscribe
+                url={postUrl}
+                render={({ subscribe, status, message }) => (
+                    <NewsLetterSignUpForm
+                        status={status}
+                        message={message}
+                        onValidated={FormData => subscribe(FormData)}
+                    />
+                )}
+            />
+        </div>
+    );
+};
+
+export default NewsLetterFormContainer;
